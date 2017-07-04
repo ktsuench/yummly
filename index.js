52286e8ac2fad3a4dbb4f5ee931c1195ff2c7039
ktsuench <will289@hotmail.com>
2017-07-04 17:50:14 -0400

    added in basic recipe search

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ var yummly = {
   config : config( pkg.config.app, pkg.api.uri, pkg.config.issue ),
   recipe : {
     search : function () {
+      var options = pkg.api.endpoint.getSearch( true );
 
+      options.path += "?" + util.buildQueryString( pkg.api.searchParams, "", "&", true );
+
+      return new Promise ( function ( resolve, reject ) {
+        https.request( options, function ( response ) {
+          if ( response.statusCode.toString().indexOf("2") === 0 ) {
+            resolve( response.body );
+          }
+        } );
+      } );
+    },
+    searchUrl : function () {
+      return pkg.api.endpoint.getSearch( false ) + "?" + util.buildQueryString( pkg.api.searchParams, "", "&", true );
     },
     get : function () {
 
